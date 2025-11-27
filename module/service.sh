ee21df1390b3a0fae4285a8c6619f4e9b3f5b2fa
KOWX712 <leecc0503@gmail.com>
2025-11-28 01:41:27 +0800

    scripts/service: set uid only when generated file avaible
    Signed-off-by: KOWX712 <leecc0503@gmail.com>

--- module/service.sh
+++ module/service.sh
@@ -5,8 +5,13 @@
 # This is free software; you can redistribute it and/or modify it under the terms of The Unlicense.
 PATH=/data/adb/ksu/bin:$PATH
 MODDIR="/data/adb/modules/ksu_switch_manager"
+KSUDIR="/data/adb/ksu"
 
-# just pull it out from /data/asystem/packages.list
-"$MODDIR/uid_tool" --setuid 12345 > /dev/null 2>&1
+if [ -f "$KSUDIR/.manager_uid" ]; then
+    uid=$(head -n1 "$KSUDIR/.manager_uid")
+
+    # just pull it out from /data/system/packages.list
+    [ -n "$uid" ] && "$MODDIR/uid_tool" --setuid $uid > /dev/null 2>&1
+fi
 
 # EOF
