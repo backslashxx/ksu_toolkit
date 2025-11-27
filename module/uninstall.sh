50b8a0cb0173d64d6c98860d21ac7fb2a78c85d8
KOWX712 <leecc0503@gmail.com>
2025-11-28 01:56:33 +0800

    scripts/uninstall: remove generated file

---
+++ module/uninstall.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# service.sh
+# No warranty.
+# No rights reserved.
+# This is free software; you can redistribute it and/or modify it under the terms of The Unlicense.
+KSUDIR="/data/adb/ksu"
+
+rm -rf "$KSUDIR/.manager_uid"
