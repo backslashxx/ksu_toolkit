b082ea53d7cf3bad18253591022f8b16112b79d7
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-12-05 16:16:32 +0800

    uid_tool: headerize

--- src/uid_tool.c
+++ src/uid_tool.c
@@ -1,69 +1,10 @@
-#include <sys/syscall.h>
 #include <stdint.h>
 #include <sys/ioctl.h>
 #include <ctype.h>
 
-// zig cc -target aarch64-linux -Oz -s -Wl,--gc-sections,--strip-all,-z,norelro -fno-unwind-tables -Wl,--entry=__start uid_tool.c -o uid_tool 
+#include "small_rt.h"
 
-// syscall de-wrappers
-#if defined(__aarch64__)
-__attribute__((noinline))
-static long __syscall(long n, long a, long b, long c, long d, long e, long f)
-{
-	register long 
-		x8 asm("x8") = n,
-		x0 asm("x0") = a,
-		x1 asm("x1") = b,
-		x2 asm("x2") = c,
-		x3 asm("x3") = d,
-		x4 asm("x4") = e,
-		x5 asm("x5") = f;
-
-	asm volatile("svc #0"
-		:"=r"(x0)
-		:"r"(x8), "r"(x0), "r"(x1), "r"(x2), "r"(x3), "r"(x4), "r"(x5)
-		:"memory");
-
-	return x0;
-}
-
-#elif defined(__arm__)
-__attribute__((noinline))
-static long __syscall(long n, long a, long b, long c, long d, long e, long f) {
-	register long
-		r7 asm("r7") = n,
-		r0 asm("r0") = a,
-		r1 asm("r1") = b,
-		r2 asm("r2") = c,
-		r3 asm("r3") = d,
-		r4 asm("r4") = e,
-		r5 asm("r5") = f;
-
-	asm volatile("svc #0"
-		: "=r"(r0)
-		: "r"(r7), "r"(r0), "r"(r1), "r"(r2), "r"(r3), "r"(r4), "r"(r5)
-		: "memory");
-
-	return r0;
-}
-
-#elif defined(__x86_64__)
-__attribute__((noinline))
-static long __syscall(long n, long a, long b, long c, long d, long e, long f) {
-	long ret;
-	asm volatile(
-		"mov %5, %%r10\n"
-		"mov %6, %%r8\n"
-		"mov %7, %%r9\n"
-		"syscall"
-		: "=a"(ret)
-		: "a"(n), "D"(a), "S"(b), "d"(c), "r"(d), "r"(e), "r"(f)
-		: "rcx", "r11", 
-		"memory");
-
-	return ret;
-}
-#endif
+// zig cc -target aarch64-linux -Oz -s -Wl,--gc-sections,--strip-all,-z,norelro -fno-unwind-tables -Wl,--entry=__start -Wno-implicit-function-declaration uid_tool.c -o uid_tool 
 
 // https://gcc.gnu.org/onlinedocs/gcc/Library-Builtins.html
 // https://clang.llvm.org/docs/LanguageExtensions.html#builtin-functions
@@ -140,7 +81,6 @@ static int show_usage(void)
 	return 1;
 }
 
-__attribute__((always_inline))
 static int c_main(int argc, char **argv, char **envp)
 {
 	const char *ok = "ok\n";
@@ -203,33 +143,3 @@ void prep_main(long *sp)
 	long exit_code = c_main(argc, argv, envp);
 	__syscall(SYS_exit, exit_code, NONE, NONE, NONE, NONE, NONE);
 }
-
-
-// arch specific small entry points
-#if defined(__aarch64__)
-__attribute__((naked))
-void __start(void) {
-	asm volatile(
-		"mov x0, sp\n"
-		"b prep_main\n"
-	);
-}
-
-#elif defined(__arm__)
-__attribute__((naked))
-void __start(void) {
-	asm volatile(
-		"mov r0, sp\n"
-		"b prep_main\n"
-    );
-}
-
-#elif defined(__x86_64__)
-__attribute__((naked, section(".text.start"))) 
-void __start(void) {
-	asm volatile(
-		"mov %rsp, %rdi\n"
-		"jmp prep_main\n"
-	);
-}
-#endif
