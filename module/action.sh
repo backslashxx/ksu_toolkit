e4b945b9f7cc09cbd463e9d90ce1c2c297ce3db2
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-05-10 13:26:44 +0800

    scripts/action: run bench on action

---
+++ module/action.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# action.sh
+# No warranty.
+# No rights reserved.
+# This is free software; you can redistribute it and/or modify it under the terms of The Unlicense.
+PATH=/data/adb/ksu/bin:$PATH
+MODDIR="/data/adb/modules/ksu_toolkit"
+KSUDIR="/data/adb/ksu"
+
+"$MODDIR/toolkit" --bench
+
+sleep 20
+
